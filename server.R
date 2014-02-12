2ca90ee253391d517a874ade26693ea5c9adfb22
mblaauw <michel.blaauw@gmail.com>
2014-02-12 09:03:54 +0100

    under constrution

--- server.R
+++ server.R
@@ -1,16 +1,9 @@
-#server.R
-#########
-
-library(shiny)
-
-
-
 shinyServer(function(input, output) {
   
-  output$medals.plot <- renderPlot({
-    packs <- c("knitr", "ggplot2", "XML", "reshape2", "rCharts")
+  output$distPlot <- renderPlot({
+    # Code adapted from http://trinkerrstuff.wordpress.com/2014/02/09/sochi-olympic-medals-2/
+    packs <- c("knitr", "ggplot2", "XML", "reshape2") #, "rCharts"
     lapply(packs, require, character.only = T)
-    
     
     olympics <- 
       function(theurl = "http://www.sochi2014.com/en/medal-standings", include.zero = FALSE) {
@@ -18,17 +11,19 @@ shinyServer(function(input, output) {
         invisible(lapply(c('ggplot2', 'XML', 'reshape2') , require, character.only=TRUE))
         
         ## Grab Data, Clean and Reshape
-        raw <- readHTMLTable(theurl, header=FALSE, 
+        raw <- readHTMLTable(theurl, header=TRUE, 
                              colClasses = c(rep("factor", 2), rep("numeric", 4)))
         raw <- as.data.frame(raw)[, -1]
-        colnames(raw) <- c("Country", "Bronze", "Silver", "Gold", "Total")
-        raw <- raw[order(raw[, "Total"]), ]
+        colnames(raw) <- c("Country",  "Gold", "Silver", "Bronze", "Total")
+        raw <- with(raw, raw[order(Total, Gold, Silver, Bronze), ])
         if (!include.zero) {
           raw <- raw[raw[, "Total"] != 0, ]
         }
         raw[, "Country"] <- factor(raw[, "Country"], levels = raw[, "Country"])
         rownames(raw) <- NULL
         mdat <- melt(raw, value.name = "Count", variable.name = "Place", id.var = "Country")
+        mdat[, "Place"] <- factor(mdat[, "Place"], 
+                                  levels = c("Bronze", "Silver", "Gold", "Total")) 
         
         ## Plot the Data
         plot1 <- ggplot(mdat, aes(x = Count, y = Country, colour = Place)) +
@@ -39,13 +34,52 @@ shinyServer(function(input, output) {
         
         return(invisible(raw))
       }
-    x <- olympics()    
-  }) 
+    
+    olympics()
+    
+  })
   
-  
-  output$medals.table <- renderTable({
-    dTable(x)$show('inline', include_assets = TRUE, cdn = TRUE)
-  }) 
+  output$distYearPlot <- renderPlot({
+    # Code adapted from http://trinkerrstuff.wordpress.com/2014/02/09/sochi-olympic-medals-2/
+    packs <- c("knitr", "ggplot2", "XML", "reshape2") #, "rCharts"
+    lapply(packs, require, character.only = T)
+    
+    url = paste('http://www.sochi2014.com/en/medal-history?year=', input$year)
 
+    olympics <- 
+      function(theurl = url, include.zero = FALSE) {
+        
+        invisible(lapply(c('ggplot2', 'XML', 'reshape2') , require, character.only=TRUE))
+        
+        ## Grab Data, Clean and Reshape
+        raw <- readHTMLTable(theurl, header=TRUE, 
+                             colClasses = c(rep("factor", 2), rep("numeric", 4)))
+        raw <- as.data.frame(raw)[, -1]
+        colnames(raw) <- c("Country",  "Gold", "Silver", "Bronze", "Total")
+        raw <- with(raw, raw[order(Total, Gold, Silver, Bronze), ])
+        if (!include.zero) {
+          raw <- raw[raw[, "Total"] != 0, ]
+        }
+        raw[, "Country"] <- factor(raw[, "Country"], levels = raw[, "Country"])
+        rownames(raw) <- NULL
+        mdat <- melt(raw, value.name = "Count", variable.name = "Place", id.var = "Country")
+        mdat[, "Place"] <- factor(mdat[, "Place"], 
+                                  levels = c("Bronze", "Silver", "Gold", "Total")) 
+        
+        ## Plot the Data
+        plot1 <- ggplot(mdat, aes(x = Count, y = Country, colour = Place)) +
+          geom_point() +
+          facet_grid(.~Place) + theme_bw()+
+          scale_colour_manual(values=c("#CC6600", "#999999", "#FFCC33", "#000000")) 
+        print(plot1)
+        
+        return(invisible(raw))
+      }
+    
+    olympics()
+    
+  })
+  
+  
   
 })
--- ui.R
+++ ui.R
@@ -1,29 +1,24 @@
-library(shiny)
-
-
-countryList <- c( "Russia", "USA", "Netherlands", "Germany", "China")
-
 shinyUI(pageWithSidebar(
-
-  headerPanel("Sochi Olympic Medals"),
+  
+  headerPanel("The Sochi 2014 Winter Olympics Medal Standings in R and RShiny"),
   
   sidebarPanel(
-    selectInput("country", "Country", countryList)
+    tags$head(
+      tags$style(type="text/css", "label.radio { display: inline-block; }", ".radio input[type=\"radio\"] { float: none; }"),
+      tags$style(type="text/css", "select { max-width: 200px; }"),
+      tags$style(type="text/css", "textarea { max-width: 185px; }"),
+      tags$style(type="text/css", ".jslider { max-width: 200px; }"),
+      tags$style(type='text/css', ".well { padding: 12px; margin-bottom: 5px; max-width: 280px; }"),
+      tags$style(type='text/css', ".span4 { max-width: 280px; }")
+    ),
+    
+    helpText("The data for this plot comes from http://www.sochi2014.com/en/medal-standings."),
+    selectInput("year", "Year:", choices = seq(1924, 2012, by=4))
   ),
   
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Medals Plot",
-               h5("Current Medals"),
-               plotOutput("medals.plot")
-      ),
-      tabPanel("Medals Table",
-               h5("Current Medals"),
-               tableOutput("medals.Table")
-      ),
-      id="tab"
-    )
+  mainPanel( 
+    plotOutput("distPlot"),
+    plotOutput("distYearPlot")
   )
   
 ))
-
